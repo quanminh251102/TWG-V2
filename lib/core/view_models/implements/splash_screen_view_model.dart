af6c3b2bbb327ef4f0408354f46b94d5d508c130
kietdev123 <79886528+kietdev123@users.noreply.github.com>
2023-11-01 07:58:25 +0700

    save token

--- lib/core/view_models/implements/splash_screen_view_model.dart
+++ lib/core/view_models/implements/splash_screen_view_model.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:twg/core/services/interfaces/iauth_service.dart';
 import 'package:twg/core/services/interfaces/isocket_service.dart';
 import 'package:twg/core/utils/token_utils.dart';
+import 'package:twg/global/global_data.dart';
 import 'package:twg/global/locator.dart';
 
 import '../../../../global/router.dart';
@@ -16,8 +17,7 @@ class SplashScreenViewModel extends ChangeNotifier
   Future<void> goToNextPage() async {
     var logined = await _iAuthService.checkLogin();
     if (logined) {
-      var token = await TokenUtils.getToken();
-      _iSocketService.connectServer(token.toString());
+      _iSocketService.connectServer(locator<GlobalData>().token);
       Get.offNamed(
         MyRouter.booking,
       );
