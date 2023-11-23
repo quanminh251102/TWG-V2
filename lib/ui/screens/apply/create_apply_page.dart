4c10dd1a14036de25fdf85dc13b7c565ace9dd86
kietdev123 <79886528+kietdev123@users.noreply.github.com>
2023-11-23 22:13:09 +0700

    Add ui

--- lib/ui/screens/apply/create_apply_page.dart
+++ lib/ui/screens/apply/create_apply_page.dart
@@ -1,244 +1,243 @@
-// import 'package:flutter/material.dart';
-// import '../../../utils/constants/colors.dart';
-// import '../../cubits/app_user.dart';
-// import '../../models/booking.dart';
-// import '../../services/apply.dart';
+import 'package:flutter/material.dart';
+import 'package:twg/core/utils/color_utils.dart';
 
-// class CreateApplyPage extends StatefulWidget {
-//   final Booking booking;
-//   const CreateApplyPage({super.key, required this.booking});
+class CreateApplyPage extends StatefulWidget {
+  // final Booking booking;
+  // const CreateApplyPage({super.key, required this.booking});
 
-//   @override
-//   State<CreateApplyPage> createState() => _CreateApplyPageState();
-// }
+  const CreateApplyPage({super.key});
 
-// enum PriceOpition { accpected, refuse }
+  @override
+  State<CreateApplyPage> createState() => _CreateApplyPageState();
+}
 
-// class _CreateApplyPageState extends State<CreateApplyPage> {
-//   PriceOpition _site = PriceOpition.accpected;
-//   late FocusNode noteFocus;
-//   late FocusNode dealPriceFocus;
-//   late TextEditingController note = TextEditingController();
-//   late TextEditingController dealPrice = TextEditingController();
-//   late GlobalKey<FormState> _formKey;
-//   bool isLoading_create_apply = false;
+enum PriceOpition { accpected, refuse }
 
-//   @override
-//   void initState() {
-//     super.initState();
-//     _site = PriceOpition.accpected;
+class _CreateApplyPageState extends State<CreateApplyPage> {
+  PriceOpition _site = PriceOpition.accpected;
+  late FocusNode noteFocus;
+  late FocusNode dealPriceFocus;
+  late TextEditingController note = TextEditingController();
+  late TextEditingController dealPrice = TextEditingController();
+  late GlobalKey<FormState> _formKey;
+  bool isLoading_create_apply = false;
 
-//     noteFocus = FocusNode();
-//     dealPriceFocus = FocusNode();
+  @override
+  void initState() {
+    super.initState();
+    _site = PriceOpition.accpected;
 
-//     noteFocus.addListener(() {
-//       setState(() {});
-//     });
+    noteFocus = FocusNode();
+    dealPriceFocus = FocusNode();
 
-//     dealPriceFocus.addListener(() {
-//       setState(() {});
-//     });
+    noteFocus.addListener(() {
+      setState(() {});
+    });
 
-//     _formKey = GlobalKey<FormState>();
-//   }
+    dealPriceFocus.addListener(() {
+      setState(() {});
+    });
 
-//   void create_apply(BuildContext context) async {
-//     setState(() {
-//       isLoading_create_apply = true;
-//     });
-//     int price = 0;
-//     if (_site == PriceOpition.accpected) {
-//       price = 0;
-//     } else {
-//       price = int.parse(dealPrice.text.trim());
-//     }
-//     String note_string = " ";
-//     if (note.text.trim().length > 0) {
-//       note_string = note.text.trim();
-//     }
-//     String result = "pass";
-//     // await Future.delayed(Duration(seconds: 2));
-//     Map<String, dynamic> _body = {
-//       'booking_id': widget.booking.id,
-//       'deal_price': price.toString(),
-//       'note': note_string,
-//       'applyer_Id': appUser.id,
-//     };
-//     result = await ApplyService.createApply(_body);
-//     if (result != "pass") {
-//       var snackBar = SnackBar(
-//         content: Text(result),
-//       );
-//       ScaffoldMessenger.of(context).showSnackBar(snackBar);
-//     } else {
-//       const snackBar = SnackBar(
-//         content: Text('Tạo apply thành công'),
-//       );
-//       ScaffoldMessenger.of(context).showSnackBar(snackBar);
-//     }
+    _formKey = GlobalKey<FormState>();
+  }
 
-//     setState(() {
-//       isLoading_create_apply = false;
-//     });
-//   }
+  void create_apply(BuildContext context) async {
+    // setState(() {
+    //   isLoading_create_apply = true;
+    // });
+    // int price = 0;
+    // if (_site == PriceOpition.accpected) {
+    //   price = 0;
+    // } else {
+    //   price = int.parse(dealPrice.text.trim());
+    // }
+    // String note_string = " ";
+    // if (note.text.trim().length > 0) {
+    //   note_string = note.text.trim();
+    // }
+    // String result = "pass";
+    // // await Future.delayed(Duration(seconds: 2));
+    // Map<String, dynamic> _body = {
+    //   'booking_id': widget.booking.id,
+    //   'deal_price': price.toString(),
+    //   'note': note_string,
+    //   'applyer_Id': appUser.id,
+    // };
+    // result = await ApplyService.createApply(_body);
+    // if (result != "pass") {
+    //   var snackBar = SnackBar(
+    //     content: Text(result),
+    //   );
+    //   ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    // } else {
+    //   const snackBar = SnackBar(
+    //     content: Text('Tạo apply thành công'),
+    //   );
+    //   ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    // }
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Tạo apply'),
-//         centerTitle: true,
-//       ),
-//       body: SingleChildScrollView(
-//         child: Padding(
-//           padding: const EdgeInsets.fromLTRB(24, 12, 24, 0),
-//           child: Form(
-//             key: _formKey,
-//             child: Column(
-//               crossAxisAlignment: CrossAxisAlignment.start,
-//               children: [
-//                 const Text('Lựa chọn giá'),
-//                 const SizedBox(height: 12),
-//                 ListTile(
-//                   title: const Text('Đồng ý giá'),
-//                   leading: Radio(
-//                     value: PriceOpition.accpected,
-//                     groupValue: _site,
-//                     onChanged: (PriceOpition? value) {
-//                       setState(() {
-//                         _site = value as PriceOpition;
-//                       });
-//                     },
-//                   ),
-//                 ),
-//                 ListTile(
-//                   title: const Text('Deal giá'),
-//                   leading: Radio(
-//                     value: PriceOpition.refuse,
-//                     groupValue: _site,
-//                     onChanged: (PriceOpition? value) {
-//                       setState(() {
-//                         _site = value as PriceOpition;
-//                         primaryFocus!.requestFocus();
-//                       });
-//                     },
-//                   ),
-//                 ),
-//                 Visibility(
-//                   visible: (_site == PriceOpition.refuse),
-//                   child: TextFormField(
-//                     keyboardType: TextInputType.number,
-//                     style: const TextStyle(fontWeight: FontWeight.bold),
-//                     focusNode: dealPriceFocus,
-//                     controller: this.dealPrice,
-//                     validator: (value) {
-//                       if (value!.isEmpty) {
-//                         return "Vui lòng nhập giá";
-//                       }
-//                       int price;
-//                       try {
-//                         price = int.parse(value);
-//                       } catch (e) {
-//                         return "Chỉ nhập kí tự số";
-//                       }
-//                       if (price == 0) {
-//                         return "Vui lòng nhập giá trị lớn hơn 0";
-//                       }
-//                       return null;
-//                     },
-//                     decoration: InputDecoration(
-//                       labelStyle: const TextStyle(fontWeight: FontWeight.w600),
-//                       focusColor: Colors.black,
-//                       filled: true, //<-- SEE HERE
-//                       fillColor: dealPriceFocus.hasFocus
-//                           ? AppColors.primaryColor.withOpacity(0.1)
-//                           : Colors.grey.withOpacity(0.1),
-//                       focusedBorder: OutlineInputBorder(
-//                         borderRadius: BorderRadius.circular(10),
-//                         borderSide:
-//                             const BorderSide(color: AppColors.primaryColor),
-//                       ),
-//                       enabledBorder: OutlineInputBorder(
-//                         borderRadius: BorderRadius.circular(10),
-//                         borderSide: BorderSide(
-//                           color: Colors.grey.withOpacity(0.1),
-//                           width: 2.0,
-//                         ),
-//                       ),
-//                       hintText: 'Giá',
-//                       prefixIcon: Icon(Icons.money,
-//                           color: dealPriceFocus.hasFocus
-//                               ? AppColors.primaryColor
-//                               : Colors.black),
-//                     ),
-//                   ),
-//                 ),
-//                 const SizedBox(height: 24),
-//                 const Text('Ghi chú'),
-//                 const SizedBox(height: 12),
-//                 TextFormField(
-//                   style: const TextStyle(fontWeight: FontWeight.bold),
-//                   focusNode: noteFocus,
-//                   controller: this.note,
-//                   validator: (value) {
-//                     // if (value!.isEmpty) {
-//                     //   return "Vui lòng nhập ghi chú";
-//                     // }
-//                     return null;
-//                   },
-//                   decoration: InputDecoration(
-//                     labelStyle: const TextStyle(fontWeight: FontWeight.w600),
-//                     focusColor: Colors.black,
-//                     filled: true, //<-- SEE HERE
-//                     fillColor: noteFocus.hasFocus
-//                         ? AppColors.primaryColor.withOpacity(0.1)
-//                         : Colors.grey.withOpacity(0.1),
-//                     focusedBorder: OutlineInputBorder(
-//                       borderRadius: BorderRadius.circular(10),
-//                       borderSide:
-//                           const BorderSide(color: AppColors.primaryColor),
-//                     ),
-//                     enabledBorder: OutlineInputBorder(
-//                       borderRadius: BorderRadius.circular(10),
-//                       borderSide: BorderSide(
-//                         color: Colors.grey.withOpacity(0.1),
-//                         width: 2.0,
-//                       ),
-//                     ),
-//                     hintText: 'Ghi chú',
-//                     prefixIcon: Icon(Icons.note_rounded,
-//                         color: noteFocus.hasFocus
-//                             ? AppColors.primaryColor
-//                             : Colors.black),
-//                   ),
-//                 ),
-//                 const SizedBox(height: 24),
-//                 Center(
-//                   child: (isLoading_create_apply)
-//                       ? const Center(
-//                           child: CircularProgressIndicator(),
-//                         )
-//                       : ElevatedButton(
-//                           onPressed: () {
-//                             if (_formKey.currentState!.validate()) {
-//                               create_apply(context);
-//                             }
-//                           },
-//                           style: ElevatedButton.styleFrom(
-//                               minimumSize: const Size(200, 50),
-//                               maximumSize: const Size(200, 50),
-//                               backgroundColor: AppColors.primaryColor),
-//                           child: const Text(
-//                             'Tạo apply',
-//                             style: TextStyle(color: Colors.white),
-//                           ),
-//                         ),
-//                 ),
-//               ],
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
+    // setState(() {
+    //   isLoading_create_apply = false;
+    // });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Tạo apply'),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(24, 12, 24, 0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text('Lựa chọn giá'),
+                const SizedBox(height: 12),
+                ListTile(
+                  title: const Text('Đồng ý giá'),
+                  leading: Radio(
+                    value: PriceOpition.accpected,
+                    groupValue: _site,
+                    onChanged: (PriceOpition? value) {
+                      setState(() {
+                        _site = value as PriceOpition;
+                      });
+                    },
+                  ),
+                ),
+                ListTile(
+                  title: const Text('Deal giá'),
+                  leading: Radio(
+                    value: PriceOpition.refuse,
+                    groupValue: _site,
+                    onChanged: (PriceOpition? value) {
+                      setState(() {
+                        _site = value as PriceOpition;
+                        primaryFocus!.requestFocus();
+                      });
+                    },
+                  ),
+                ),
+                Visibility(
+                  visible: (_site == PriceOpition.refuse),
+                  child: TextFormField(
+                    keyboardType: TextInputType.number,
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                    focusNode: dealPriceFocus,
+                    controller: this.dealPrice,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Vui lòng nhập giá";
+                      }
+                      int price;
+                      try {
+                        price = int.parse(value);
+                      } catch (e) {
+                        return "Chỉ nhập kí tự số";
+                      }
+                      if (price == 0) {
+                        return "Vui lòng nhập giá trị lớn hơn 0";
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                      labelStyle: const TextStyle(fontWeight: FontWeight.w600),
+                      focusColor: Colors.black,
+                      filled: true, //<-- SEE HERE
+                      fillColor: dealPriceFocus.hasFocus
+                          ? ColorUtils.primaryColor.withOpacity(0.1)
+                          : Colors.grey.withOpacity(0.1),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide:
+                            const BorderSide(color: ColorUtils.primaryColor),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(
+                          color: Colors.grey.withOpacity(0.1),
+                          width: 2.0,
+                        ),
+                      ),
+                      hintText: 'Giá',
+                      prefixIcon: Icon(Icons.money,
+                          color: dealPriceFocus.hasFocus
+                              ? ColorUtils.primaryColor
+                              : Colors.black),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 24),
+                const Text('Ghi chú'),
+                const SizedBox(height: 12),
+                TextFormField(
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                  focusNode: noteFocus,
+                  controller: this.note,
+                  validator: (value) {
+                    // if (value!.isEmpty) {
+                    //   return "Vui lòng nhập ghi chú";
+                    // }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    labelStyle: const TextStyle(fontWeight: FontWeight.w600),
+                    focusColor: Colors.black,
+                    filled: true, //<-- SEE HERE
+                    fillColor: noteFocus.hasFocus
+                        ? ColorUtils.primaryColor.withOpacity(0.1)
+                        : Colors.grey.withOpacity(0.1),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide:
+                          const BorderSide(color: ColorUtils.primaryColor),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(
+                        color: Colors.grey.withOpacity(0.1),
+                        width: 2.0,
+                      ),
+                    ),
+                    hintText: 'Ghi chú',
+                    prefixIcon: Icon(Icons.note_rounded,
+                        color: noteFocus.hasFocus
+                            ? ColorUtils.primaryColor
+                            : Colors.black),
+                  ),
+                ),
+                const SizedBox(height: 24),
+                Center(
+                  child: (isLoading_create_apply)
+                      ? const Center(
+                          child: CircularProgressIndicator(),
+                        )
+                      : ElevatedButton(
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              create_apply(context);
+                            }
+                          },
+                          style: ElevatedButton.styleFrom(
+                              minimumSize: const Size(200, 50),
+                              maximumSize: const Size(200, 50),
+                              backgroundColor: ColorUtils.primaryColor),
+                          child: const Text(
+                            'Tạo apply',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/ui/utils/handling_string_utils.dart
+++ lib/ui/utils/handling_string_utils.dart
@@ -3,4 +3,117 @@ class HandlingStringUtils {
     if (str.length > 25) return '${str.substring(0, 20)}...';
     return str;
   }
+
+  static String validateForLongString(String str) {
+    /*
+       - INSTRUCTION
+       double str = "string long long long long long long long long"
+       String result = validateForLongString(str)
+       => result = "string long long long ...."
+    */
+    String res = str;
+    if (str.length > 20) res = '${str.substring(0, 17)} ...';
+    return res;
+  }
+
+  static String validateForLongStringWithLim(String str, int lim) {
+    /*
+       - INSTRUCTION
+       double str = "string long long long long long long long long"
+       String result = validateForLongString(str)
+       => result = "string long long long ...."
+    */
+    String res = str;
+    if (str.length > lim) res = '${str.substring(0, lim - 3)} ...';
+    return res;
+  }
+
+  static String priceWithValue(double price) {
+    /*
+       - INSTRUCTION
+       double price = 6200000
+       String result = priceWithValue(price)
+       => result = 6tr200k
+    */
+
+    String res = '';
+    int value = price.toInt();
+    if (value >= 1000000) {
+      res = '$res${(value ~/ 1000000).toString()}tr';
+    }
+    value = value % 1000000;
+    if (value > 0) res = '$res${(value ~/ 1000).toString()}k';
+    if (res == '') res = '0k';
+
+    return res;
+  }
+
+  static String priceInPost_noType(String price) {
+    /*
+    - INSTRUCTION
+      post = Post(
+        id: 'p1',
+        userId: 'user-1',
+        description: ...
+        emotion: ['tiêu cực'],
+        attachmentsURL: ...
+        price: 6200000,
+        time: "15",
+        content: "Tôi ế quá cần sớm có ny",
+        isFullTime: false,
+        isDate: false,
+        isHour: false,
+        isMonth: true,
+        isYear: false,
+      );
+      String result = priceInPost(post);
+      => res = '6tr200k/tháng'
+    */
+    String res = '';
+    int value = int.parse(price);
+    if (value >= 1000000) {
+      res = res = '$res${(value ~/ 1000000).toString()}tr';
+    }
+    value = value % 1000000;
+    if (value > 0) res = '$res${(value ~/ 1000).toString()}k';
+
+    if (res == '') res = '0k';
+
+    return res;
+  }
+
+  static String timeDistanceFromNow(DateTime dateTime) {
+    /*
+    - INSTRUCTION
+
+    res = timeDistanceFromNow(post.time);
+    => res = '1 năm trước' 
+            or '300 ngày trước' 
+            or '2 giờ trước'
+            or '2 phút trước'
+            or '2 guây trước'
+    */
+    DateTime now = DateTime.now();
+    String res = 'error';
+    Duration diff = now.difference(dateTime);
+    if (diff.inDays > 0) {
+      if (diff.inDays >= 365) {
+        res = '${(diff.inDays ~/ 365).toInt().toString()} năm';
+      } else {
+        res = '${diff.inDays.toInt().toString()} ngày';
+      }
+    } else {
+      if (diff.inHours > 0) {
+        res = '${diff.inHours.toInt().toString()} giờ';
+      } else {
+        if (diff.inMinutes > 0) {
+          res = '${diff.inMinutes.toInt().toString()} phút';
+        } else {
+          res = '${diff.inSeconds.toInt().toString()} giây';
+        }
+      }
+    }
+    res = '$res trước';
+    return res;
+  }
 }
